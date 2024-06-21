9ed818b5c73b7dedb688ef12c8eec3c5a6b677b6
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-06-21 11:49:28 +0300

    chore: add OCTO_LOGGER_NAME

--- octo/logging/general.py
+++ octo/logging/general.py
@@ -1,12 +1,12 @@
 import logging
 import logging.config
-from octo.logging.config import OCTO_LOGGING
+from octo.logging.config import OCTO_LOGGING, OCTO_LOGGER_NAME
 
 
 class Logger:
     def __init__(self) -> None:
         self._config = OCTO_LOGGING
-        self._loging_name = "octo-log"
+        self._loging_name = OCTO_LOGGER_NAME
 
     def get(self) -> logging:
         logging.config.dictConfig(self._config)
