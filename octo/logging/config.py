3ae37125261f660a8f80b0579ba767e3ec7b6605
Wissam Al-Kahwaji <wissamalkahwaji@gmail.com>
2024-07-05 11:30:17 +0300

    feat: change variable to function

--- octo/logging/config.py
+++ octo/logging/config.py
@@ -6,37 +6,39 @@ try:
 except AttributeError:
     OCTO_LOGGER_NAME = "octo-log"
 
-OCTO_LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "colored": {
-            "()": "colorlog.ColoredFormatter",
-            "format": "%(log_color)s[%(asctime)s] [%(process)d] [%(module)s] [%(levelname)s] %(message)s %(reset)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-            "log_colors": {
-                "DEBUG": "cyan",
-                "INFO": "green",
-                "WARNING": "yellow",
-                "ERROR": "red",
-                "CRITICAL": "bold_red",
+
+def get_logging(name: str | None = OCTO_LOGGER_NAME):
+    return name, {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "colored": {
+                "()": "colorlog.ColoredFormatter",
+                "format": "%(log_color)s[%(asctime)s] [%(process)d] [%(module)s] [%(levelname)s] %(message)s %(reset)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+                "log_colors": {
+                    "DEBUG": "cyan",
+                    "INFO": "green",
+                    "WARNING": "yellow",
+                    "ERROR": "red",
+                    "CRITICAL": "bold_red",
+                },
+                "style": "%",
             },
-            "style": "%",
         },
-    },
-    "handlers": {
-        f"{OCTO_LOGGER_NAME}": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": os.path.join(settings.BASE_DIR, f"logs/{OCTO_LOGGER_NAME}.log"),
-            "formatter": "colored",
+        "handlers": {
+            f"{name}": {
+                "level": "DEBUG",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(settings.BASE_DIR, f"logs/{name}.log"),
+                "formatter": "colored",
+            },
         },
-    },
-    "loggers": {
-        f"{OCTO_LOGGER_NAME}": {
-            "handlers": [f"{OCTO_LOGGER_NAME}"],
-            "level": "DEBUG",
-            "propagate": True,
+        "loggers": {
+            f"{name}": {
+                "handlers": [f"{name}"],
+                "level": "DEBUG",
+                "propagate": True,
+            },
         },
-    },
-}
+    }
